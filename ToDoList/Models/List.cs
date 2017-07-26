e430a95b846d73619922240369407f5ea459e67a
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-26 14:34:59 -0400

    stretch tasks

---
+++ ToDoList/Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; } //int tytpe auto-generates ID
+        [Display(Name = "Title")]
+        public string ListTitle { get; set; }
+        public DateTime ListCreateDate { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; } //each list has multiple tasks, establishes relationship between list and task model
+    }
+}
---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskID { get; set; }
+        [Display(Name = "Task Name")]
+        public int TaskName { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Is Done")]
+        public bool IsDone { get; set; }
+        [Display(Name = "Due Date")]
+        public DateTime DueDate { get; set; }
+
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
+
+    }
+}
